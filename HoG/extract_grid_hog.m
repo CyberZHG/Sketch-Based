92ec545de6134f565f1c8984c7de9e8698eafac6
cyberzhg <cyberzhg@gmail.com>
2014-12-06 15:35:12 +0800

    Independent.
    Signed-off-by: cyberzhg <cyberzhg@gmail.com>

--- HoG/extract_grid_hog.m
+++ HoG/extract_grid_hog.m
@@ -1,20 +1,23 @@
 function Feature = extract_grid_hog(Image, Num, Bin)
     % Feature: (Num * Num * Bin) x 1 vector.
     if nargin == 2
-        Bin = 8;
+        Bin = 9;
     end
     Height = size(Image, 1);
     Width = size(Image, 2);
     RowLength = Height / Num;
     ColLength = Width / Num;
-    Feature = zeros([Bin * Num * Num, 1]);
-    for R = 1 : Num
-        for C = 1 : Num
-            Patch = Image(floor((R - 1) * RowLength) + 1 : floor(R * RowLength), ...
-                          floor((C - 1) * ColLength) + 1 : floor(C * ColLength));
-            Start = ((R - 1) * Num + C - 1) * Bin + 1;
-            End = Start + Bin - 1;
-            Feature(Start : End) = extract_hog(Patch, Bin);
+    Feature = zeros([Num * Num * Bin, 1]);
+    Px = imfilter(Image, [-1 -2 -1; 0 0 0; 1 2 1]);
+    Py = imfilter(Image, [-1 0 1; -2 0 2; -1 0 1]);
+    Amplitude = sqrt(Px .* Px + Py .* Py);
+    Angle = mod(floor((atan2(Py, Px) + pi * 4) / (pi / Bin)), Bin);
+    for i = 1 : Height
+        for j = 1 : Width
+            Row = floor((i - 1) / RowLength);
+            Col = floor((j - 1) / ColLength);
+            Index = (Row * Num + Col) * Bin + Angle(i, j) + 1;
+            Feature(Index) = Feature(Index) + Amplitude(i, j);
         end
     end
 end
