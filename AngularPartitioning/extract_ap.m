99b9bfb58386e37fbe224c2c7056d0e73de2b04f
cyberzhg <cyberzhg@gmail.com>
2014-12-15 21:02:36 +0800

    Divide the distance to the center to some bins.
    Signed-off-by: cyberzhg <cyberzhg@gmail.com>

--- AngularPartitioning/extract_ap.m
+++ AngularPartitioning/extract_ap.m
@@ -1,15 +1,22 @@
-function Feature = extract_ap(Sketch, BinNum)
-    if nargin < 2
-        BinNum = 12;
+function Feature = extract_ap(Sketch, AngleBinNum, NormBinNum)
+    if nargin == 1
+        AngleBinNum = 12;
+        NormBinNum = 5;
     end
     if size(Sketch, 3) > 1
         Sketch = rgb2gray(Sketch);
     end
     Sketch = im2double(Sketch);
-    CenterX = size(Sketch, 1) * 0.5;
-    CenterY = size(Sketch, 2) * 0.5;
-    AngleLen = pi * 2.0 / BinNum;
+    Height = size(Sketch, 1);
+    Width = size(Sketch, 2);
+    CenterX = Height * 0.5;
+    CenterY = Width * 0.5;
+    AngleLen = pi * 2.0 / AngleBinNum;
+    NormLen = max([CenterX, CenterY]) * sqrt(2.0) / NormBinNum  + 0.01;
     [Rows, Cols] = find(Sketch > 0.5);
-    Index = mod(floor((atan2(Cols - CenterY, Rows - CenterX) + 4 * pi) / AngleLen), BinNum) + 1;
-    Feature = abs(fft(hist(Index, BinNum)' / length(Index)));
+    AngleBin = mod(floor((atan2(Cols - CenterY, Rows - CenterX) + 4 * pi) / AngleLen), AngleBinNum);
+    NormBin = floor(sqrt((Rows - CenterX) .^ 2 + (Cols - CenterY) .^ 2) / NormLen);
+    Index = NormBin * AngleBinNum + AngleBin + 1;
+    Histogram = reshape(hist(Index, AngleBinNum * NormBinNum), [AngleBinNum, NormBinNum])/ length(Rows);
+    Feature = reshape(abs(fft(Histogram)), [AngleBinNum * NormBinNum, 1]);
 end
--- AngularPartitioning/test_ap.m
+++ AngularPartitioning/test_ap.m
@@ -1,24 +1,25 @@
-function Results = test_ap(Datasets, Queries, BinNum)
+function Results = test_ap(Datasets, Queries, AngleBinNum, NormBinNum)
     % Datasets: N x 1 cells containing location of dataset images.
     % Queries: M x 1 cells containing location of query images.
     % Results: N x M matrix, each column contains the ranking of the
     % dataset images.
     if nargin == 2
-        BinNum = 12;
+        AngleBinNum = 12;
+        NormBinNum = 5;
     end
     N = length(Datasets);
     M = length(Queries);
-    Features = zeros([BinNum, N]);
+    Features = zeros([AngleBinNum * NormBinNum, N]);
     parfor i = 1 : N
         Image = imread(Datasets{i});
         Image = im2double(Image);
-        Features(:, i) = extract_ap(Image, BinNum);
+        Features(:, i) = extract_ap(Image, AngleBinNum, NormBinNum);
     end
     Results = zeros([N, M]);
     for i = 1 : M
         Image = imread(Queries{i});
         Image = im2double(Image);
-        Feature = extract_ap(Image, BinNum);
+        Feature = extract_ap(Image, AngleBinNum, NormBinNum);
         Dist = zeros([N, 1]);
         parfor j = 1 : N
             Dist(j) = distance_ap(Feature, Features(:, j));
